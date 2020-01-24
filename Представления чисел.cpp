80f0409437899867a911edc8c7116d5645a1765c
zimka1 <zimkail2015@mail.ru>
2020-01-24 18:19:07 +0300

    s commit

--- Представления чисел.cpp
+++ Представления чисел.cpp
@@ -1,3 +1,22 @@
+Skip to content
+Search or jump to…
+
+Pull requests
+Issues
+Marketplace
+Explore
+
+@zimka1
+zimka1
+/
+-
+1
+00
+ Code Issues 0 Pull requests 0 Actions Projects 0 Wiki Security Insights Settings
+-/Представления чисел.cpp
+@zimka1 zimka1 Update Представления чисел.cpp
+f796c42 9 hours ago
+115 lines (105 sloc)  2.32 KB
 
 #include <bits/stdc++.h>
 #include <ext/pb_ds/assoc_container.hpp>
@@ -16,12 +35,12 @@ using namespace std;
 using namespace __gnu_pbds;
 
 typedef tree<
-        ii,
-        null_type,
-        less<ii>,
-        rb_tree_tag,
-        tree_order_statistics_node_update>
-        ordered_set;
+ii,
+null_type,
+less<ii>,
+rb_tree_tag,
+tree_order_statistics_node_update>
+ordered_set;
 const int N=1e5 +100;
 
 char p[N][3];
@@ -31,15 +50,27 @@ main () {
     cin.tie(0);
     cout.tie(0);
     ios_base::sync_with_stdio(0);
+#ifdef zimka
 
-ii n;
-cin >> n;
+    freopen("input.txt","r",stdin);
+    freopen("output.txt","w",stdout);
+#else
+
+    //    freopen("input.txt","r",stdin);
+    //    freopen("output.txt","w",stdout);
+#endif
+    ii n;
+    cin >> n;
     ii d;
     cin >> d;
     ii x=n;
+    ii t=0;
+    if (d==1) {
+        d=2;
+        t=1;
+    }
     string s="";
-    while (x!=0)
-    {
+    while (x!=0) {
         s+=to_string(x%d);
         x/=d;
     }
@@ -47,27 +78,59 @@ cin >> n;
     cout<<s<<endl;
     string ans="";
     string s2="";
-    for (ii i = 0 ;i < s.size(); i++)
-    {
+    for (ii i = 0 ; i < s.size(); i++) {
         string s1="";
         s1+=s[i];
         ii y=atoi(s1.c_str());
-        if (i==s.size()-1)continue;
-        if (i==0)ans+=to_string(d);
-        else {
-            ans += '*';
-            ans += to_string(d);
-
-            while (y != 0) {
+        if (i==s.size()-1)
+            continue;
+        if (t==1)
+            d=1;
+        if (i==0) {
+            if (t==1) {
+                ans += '(';
+                ans += to_string(d);
                 ans += '+';
                 ans += to_string(d);
+                ans += ')';
+            } else
+                ans+=to_string(d);
+
+        } else {
+            if (t==1) {
+                ans += '*';
+                ans += '(';
+                ans += to_string(d);
+                ans += '+';
+                ans += to_string(d);
+                ans += ')';
+            } else {
+                ans +='*';
+                ans += to_string(d);
+            }
+            while (y != 0) {
+                if (t==1) {
+                    ans += '+';
+                    ans += '(';
+                    ans += to_string(d);
+                    ans += '+';
+                    ans += to_string(d);
+                    ans += ')';
+                }
+
+                else {
+                    ans +='+';
+                    ans += to_string(d);
+                }
                 y--;
             }
         }
-            ans += ')';
+        ans += ')';
 
         s2+='(';
     }
     cout<<s2+ans;
-
+    if (t==1&&n%2!=0)
+        cout<<"+1";
 }
+
