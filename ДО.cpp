0a21ffae80eccbd60e3431c2144bb2b4e825b463
zimka1 <56439181+zimka1@users.noreply.github.com>
2020-11-05 09:20:33 +0300

    Update ДО.cpp

--- ДО.cpp
+++ ДО.cpp
@@ -30,95 +30,96 @@
 
 using namespace std;
 
-vector <pair<ii,ii>> t(400000,{0,0});
-void build (ii v,ii vl,ii vr,ii a[])
+vector <ii> t(4000000);
+//void build (ii v,ii vl,ii vr,ii a[])
+//{
+//    if (vl==vr)
+//    {
+//        t[v]=a[vl];
+//        t[v]%=1337;
+//        return;
+//    }
+//    if (vl > vr) return ;
+//    ii mid=vl+(vr-vl)/2;
+//    build(2*v+1,vl,mid,a);
+//    build(2*v+2,mid+1,vr,a);
+//    t[v] = t[v*2 + 2] + t[v*2+1];
+//    t[v]%=1337;
+////    t[v]=max(t[2*v+1],t[2*v+2]);
+//
+//}
+ii sum(ii v,ii vl,ii vr,ii l,ii r)
 {
-    if (vl==vr)
+    if (r<vl||vr<l || vl > vr)
     {
-        t[v].F=a[vl];
-        t[v].S=vl;
-        return;
+        return 0;
     }
-    ii mid=vl+(vr-vl)/2;
-    build(2*v+1,vl,mid,a);
-    build(2*v+2,mid+1,vr,a);
-    if (t[2*v+1].F>=t[2*v+2].F)t[v]=t[2*v+1];
-    else
-        t[v]=t[2*v+2];
-//    t[v]=max(t[2*v+1],t[2*v+2]);
-
-}
-pair<ii,ii> qe(ii v,ii vl,ii vr,ii l,ii r)
-{
-if (r<vl||vr<l)
-{
-    return {-1e9,-1};
-}
-if (l<=vl&&vr<=r)
-{
-    return t[v];
-}
-ii mid =vl+(vr-vl)/2;
-pair<ii,ii> ql = qe(2*v+1,vl,mid,l,r);
-pair<ii,ii> qr = qe(2*v+2,mid+1,vr,l,r);
-if (ql.F>=qr.F)
-    return ql;
-else
-    return qr;
-
-}
-void mod (ii v, ii vl, ii vr, ii pos, ii val)
-{
-    if (vl==vr){
-        t[v].F=val;
-        return;
+    if (l<=vl&&vr<=r)
+    {
+        return t[v];
     }
-    ii mid = vl+(vr-vl)/2;
-    if (pos<=mid)
-        mod (2*v+1,vl,mid,pos,val);
-    else
-        mod (2*v+2,mid+1,vr,pos,val);
-       if (t[2*v+1].F>=t[2*v+2].F)t[v]=t[2*v+1];
-    else
-        t[v]=t[2*v+2];
+    ii mid =(vl + vr)/2;
+    return (sum(v*2,vl,mid,l,r) + sum(v*2+1,mid+1,vr,l,r))%1337;
+
+}
+void upd (ii v, ii val)
+{
+    t[v] = val;
+    v /= 2;
+    while(v){
+        t[v] = t[v * 2] + t[v * 2 + 1];
+        v /= 2;
+    }
 
 }
 
-int main () {
+int main ()
+{
 
     cin.tie(0);
     cout.tie(0);
     ios_base::sync_with_stdio(0);
-    freopen("input.txt","r",stdin);
- freopen("output.txt","w",stdout);
-ii n;
-cin >> n;
-ii a[n];
-for (ii i = 0; i < n ;i++)
-{
-    cin >> a[i];
-}
-build (0,0,n-1,a);
-ii q;
-cin >> q;
-for (ii i = 0; i <q;i++ )
-{
-    char c;
-    cin >> c;
-    if (c=='s')
-        {
-    ii l,r;
-    cin >> l >> r;
-    l--;
-    r--;
-        cout<<qe(0,0,n-1,l,r).S+1<<' ';
-        }
-    else
+//    freopen("input.txt","r",stdin);
+//    freopen("output.txt","w",stdout);
+    ii n;
+    cin >> n;
+    ii q;
+    cin >> q;
+    ii a[n];
+    ii st = 1;
+    while (st < n)
     {
-        ii pos,val;
-        cin >> pos>> val;
-        pos--;
-        mod(0,0,n-1,pos,val);
+        st*=2;
     }
-}
+    for (ii i = 0; i < n ; i++)
+    {
+        cin >> a[i];
+        t[i+st] = a[i];
+    }
+    for (ii i = st - 1; i >= 0; i--)
+    {
+        t[i] = t[i*2] + t[i*2+1];
+        t[i]%=1337;
+    }
+    for (ii i = 0; i <q; i++)
+    {
+        string c;
+        cin >> c;
+        if (c=="sum")
+        {
+            ii l,r;
+            cin >> l >> r;
+            l--;
+            r--;
+            ii s = sum(1,0,st-1,l,r);
+            cout<<(((s*(s+1))/2)%1337)*(((s*(s+1))/2)%1337) % 1337<<endl;
+        }
+        else
+        {
+            ii pos,val;
+            cin >> pos>> val;
+            pos--;
+            upd(pos + st, val);
+        }
+    }
 }
